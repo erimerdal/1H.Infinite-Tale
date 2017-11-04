ea1ba9ae581ac7653778b1b07f53eda63c0afba3
huseyintaskesen <huseyintaskesen1@gmail.com>
2017-11-04 20:45:05 +0300

    Add files via upload

---
+++ src/ui/SettingsManager.java
@@ -0,0 +1,15 @@
+package UI;
+
+public class SettingsManager {
+    String userName;
+    SettingsMenu menu = new SettingsMenu();
+
+    public void setUsername()
+    {
+        userName = SettingsMenu.username;
+    }
+    public String getUsername(){
+        return userName;
+    }
+
+}
